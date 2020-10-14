eb8f89ba5a25fd651c872f561e4c52164a1ac0c3
Andreas Feser <a.feser@schule.koeln>
2020-10-14 09:18:38 +0200

    created model (question, idea, keyword)

--- DJenv/Scripts/example/lightning/ideas/models.py
+++ DJenv/Scripts/example/lightning/ideas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+class Question(models.Model):
+    text = models.CharField(max_length=100)
+    author = models.CharField(max_length=20)
+    end = models.DateTimeField()
+    lifespan_ideas = models.DurationField()
+
+    def __str__(self):
+        return '{} fragt: {}'.format(self.author, self.text)
+
+class Idea(models.Model):
+    test = models.CharField(max_length=200),
+    end = models.DateTimeField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+class Keyword(models.Model):
+    text = models.CharField(max_length=20)
+
 # Create your models here.
